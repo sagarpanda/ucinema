1bd10ed361814f107fa3fa7539c56bef81b76fff
sagarpanda <er.sagarpanda@gmail.com>
2020-03-30 05:46:28 +0000

    Deploying to gh-pages from  - 48934b28b9fe6a8bd7fff179a7b6443cfda51fdc 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aae81600bac942fb310c7f070210aa6b.js"
+  "./precache-manifest.5268dcbc5c479fc5eec27a9204207590.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
