d4568e900fb723ccbfe7c99338287ea138273c0f
Sagar Panda <er.sagarpanda@gmail.com>
2020-06-14 23:08:23 +0530

    updated config

--- packages/cinema-ui/src/config.js
+++ packages/cinema-ui/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  intraBaseUrl: 'https://192.168.29.102:9000',
+  intraBaseUrl: 'https://192.168.29.233:9000',
   googleSheetId: '1xqxT4Q0Zh4n9W5GYew-ltxfb4oG42icVlHQEX_RErQE',
   checkerUrl: '/checker',
   movieUrl: '/movies'
