64e7449976d3cc5a740d180b08a13d59fd0ed53e
Sagar Panda <er.sagarpanda@gmail.com>
2021-11-21 01:24:05 +0530

    updated api url

--- packages/cinema-ui/src/utils/fetchGoogleSheet.js
+++ packages/cinema-ui/src/utils/fetchGoogleSheet.js
@@ -1,27 +1,13 @@
-async function fetchGoogleSheet({spreadsheetId, sheetNum}) {
-  const tab = sheetNum || 1;
-  const fetchUrl = `https://spreadsheets.google.com/feeds/cells/${spreadsheetId}/${tab}/public/full?alt=json`;
-  const data = await fetch(fetchUrl).then(response => {
-    if(response.status === 200) {
-      return response.json();
-    } else {
-      return { feed: { entry: [] }};
-    }
-  });
-  // let count = 0;
-  const rowData = data.feed.entry.reduce((prev, curr) => {
-     const { row, col } = curr['gs$cell'];
-     const content = curr.content['$t'];
-     const rowIndex = row - 1;
-     const colIndex = col - 1;
-     if(prev[rowIndex]) {
-       prev[rowIndex][`col${colIndex}`] = content;
-     } else {
-       prev[rowIndex] = { [`col${colIndex}`]: content };
-     }
-     return prev;
-  }, []);
-  return rowData;
+async function fetchGoogleSheet() {
+	const fetchUrl = `https://mock-serv.herokuapp.com/api/cinema`;
+	const data = await fetch(fetchUrl).then((response) => {
+		if (response.status === 200) {
+			return response.json();
+		} else {
+			return { records: [] };
+		}
+	});
+	return data.records;
 }
 
 export default fetchGoogleSheet;
